363626802a422d011dc6d535521b5367fa17fd2e
Sunil Kumar <sunilk10099@gmail.com>
2022-07-12 17:00:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function Book() {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = () => {
+    read_str = "";
+    if (this.read) {
+      read_str = "already read";
+    } else {
+      read_str = "not read yet";
+    }
+    return (
+      this.title + " by " + this.author + ", " + this.pages + ", " + read_str
+    );
+  };
+}
+
+
