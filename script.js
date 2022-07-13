c5a92966204055e83bbd90ce5d1df3f249d703d5
Sunil Kumar <sunilk10099@gmail.com>
2022-07-13 20:43:59 +0530

    Add functions for display and adding book

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = () => {
-    read_str = "";
+  this.getTitle = () => {
+    return this.title;
+  };
+  this.getAuthor = () => {
+    return this.author;
+  };
+  this.getPages = () => {
+    return this.pages;
+  };
+  this.getRead = () => {
     if (this.read) {
-      read_str = "already read";
+      return "Already read the book!";
     } else {
-      read_str = "not read yet";
+      return "Not read yet.";
     }
-    return (
-      this.title + " by " + this.author + ", " + this.pages + ", " + read_str
-    );
   };
 }
 
-function addBookToLibrary() {
-  
+function addBookToLibrary(...args) {
+  const book = new Book(...args);
+  myLibrary.push(book);
+}
+
+function displayBook() {
+  myLibrary.forEach((book) => {
+    const container = document.getElementById("book_container");
+    const div = document.createElement("div");
+    div.setAttribute("class", "book_card");
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const read = document.createElement("p");
+    title.setAttribute("class", "title");
+    author.setAttribute("class", "author");
+    pages.setAttribute("class", "pages");
+    read.setAttribute("class", "read");
+    title.innerHTML = "<span>Title : </span> " + book.getTitle();
+    author.innerHTML = "<span>Author : </span> " + book.getAuthor();
+    pages.innerHTML = "<span>Total Pages : </span> " + book.getPages();
+    read.innerHTML = book.getRead();
+    div.appendChild(title);
+    div.appendChild(author);
+    div.appendChild(pages);
+    div.appendChild(read);
+    container.appendChild(div);
+  });
 }
